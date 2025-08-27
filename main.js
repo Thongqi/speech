0a3ea23633b0c3c9d1519254f6ea483e8f3520d8
Thongqi <141812298+Thongqi@users.noreply.github.com>
2025-08-27 17:54:00 +0800

    add in small upload button, and remove big upload div after upload, create customization for lineheight and fontsize

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
 document.addEventListener("DOMContentLoaded", (event) => { 
     const speech = document.querySelector('#speech')
     speech.addEventListener('change', uploadSpeech)
+
+    const small_upload = document.querySelector('.upload_speech_small')
+    small_upload.addEventListener('click', toggleBigUpload)
 })
 
+function lineHeightSlider(){
+    const lineHeight = document.querySelector('.line_height_controller')
+    lineHeight.addEventListener('input', (event) => {
+        setLineHeight(event.target.value)
+    })
+}
+
+function setLineHeight(lineHeight){
+    document.querySelector('.displaySpeech').style.lineHeight = lineHeight;
+}
+
+function fontSizeSlider(){
+    const fontSize = document.querySelector('.font_size_controller')
+    fontSize.addEventListener('input', (event) => {
+        setfontSize(event.target.value)
+    })
+}
+
+function setfontSize(fontSize){
+    document.querySelector('.displaySpeech').style.fontSize = fontSize;
+}
 
 function uploadSpeech(event){
     event.preventDefault();
@@ -19,5 +43,16 @@ function uploadSpeech(event){
             });
     };
 
+    toggleBigUpload();
     reader.readAsArrayBuffer(event.target.files[0]);
 };
+
+function toggleBigUpload(){
+    const bigUpload = document.querySelector('.upload_speech_big')
+    if (bigUpload.style.display == 'none'){
+        bigUpload.style.display = 'block';
+    } else {
+        bigUpload.style.display = 'none';
+    }
+}
+
