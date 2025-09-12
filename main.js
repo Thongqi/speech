171d0cd52656c99d9454dd9780c24e7e1e3b27d1
Thongqi <141812298+Thongqi@users.noreply.github.com>
2025-09-12 15:26:33 +0800

    add in similarity function

--- main.js
+++ main.js
@@ -23,12 +23,12 @@ function spokeString(string){
     var remainingSpace = document.querySelector('.read').nextSibling.nodeValue.split(' ').length;
     // if next string length not less than 3, 
     var nextString;
-    if (document.querySelector('.toread')){
-        if(remainingSpace < 2){
+    if (document.querySelector('.toread') && remainingSpace < 5){
+        if(remainingSpace < 2 && remainingSpace >5){
             var addOn = document.querySelector('.read').nextSibling.nextSibling.nextSibling.nodeValue.split(' ').slice(0,2).join(' ');
             nextString = document.querySelector('.read').nextSibling.nextSibling.innerHTML.split(' ').slice(1).join(' ') + addOn;
         } else {
-            nextString = spokeStringElement.nextSibling.nextSibling.innerHTML.split(' ').slice(1,4).join(' ');
+            nextString = spokeStringElement.nextSibling.nextSibling.innerHTML
         }
     } else{
         nextString = spokeStringElement.nextSibling.nodeValue.split(' ').slice(1,4).join(' ')
@@ -44,6 +44,47 @@ function setLineHeight(lineHeight){
 
 function setFontSize(fontSize){
     document.querySelector('.display_speech').style.fontSize = `${fontSize}px`;
+}
+
+function similarity(s1, s2) {
+  var longer = s1;
+  var shorter = s2;
+  if (s1.length < s2.length) {
+    longer = s2;
+    shorter = s1;
+  }
+  var longerLength = longer.length;
+  if (longerLength == 0) {
+    return 1.0;
+  }
+  return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
+}
+
+function editDistance(s1, s2) {
+  s1 = s1.toLowerCase();
+  s2 = s2.toLowerCase();
+
+  var costs = new Array();
+  for (var i = 0; i <= s1.length; i++) {
+    var lastValue = i;
+    for (var j = 0; j <= s2.length; j++) {
+      if (i == 0)
+        costs[j] = j;
+      else {
+        if (j > 0) {
+          var newValue = costs[j - 1];
+          if (s1.charAt(i - 1) != s2.charAt(j - 1))
+            newValue = Math.min(Math.min(newValue, lastValue),
+              costs[j]) + 1;
+          costs[j - 1] = lastValue;
+          lastValue = newValue;
+        }
+      }
+    }
+    if (i > 0)
+      costs[s2.length] = lastValue;
+  }
+  return costs[s2.length];
 }
 
 function checkFileExtension(event){
